0fc7bc489ea0a472f3d9b65c4a8f379e256dac96
Lucas Souza <lucas23500@gmail.com>
2024-03-27 20:31:50 -0400

    Aula 20/07/2024 - Agora podemos ter uma segunda tela mostrando os filmes

--- app/src/main/java/br/edu/ifam/daic/filmes/recycler/MovieAdapter.java
+++ app/src/main/java/br/edu/ifam/daic/filmes/recycler/MovieAdapter.java
@@ -1,5 +1,7 @@
 package br.edu.ifam.daic.filmes.recycler;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
@@ -7,25 +9,32 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
+import br.edu.ifam.daic.filmes.R;
 import br.edu.ifam.daic.filmes.entity.Movie;
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieViewHolder> {
 
-
+    public List<Movie> movieList;
+    public MovieAdapter(List<Movie> movieList) {
+        this.movieList = movieList;
+    }
 
     @NonNull
     @Override
     public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_movie, parent, false);
+        return new MovieViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
-
+        Movie movie = movieList.get(position);
+        holder.getTextViewMovie().setText(movie.getTitulo() + " - " +
+                Integer.toString(movie.getAno()));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return movieList.size();
     }
 }
--- app/src/main/java/br/edu/ifam/daic/filmes/recycler/MovieViewHolder.java
+++ app/src/main/java/br/edu/ifam/daic/filmes/recycler/MovieViewHolder.java
@@ -1,14 +1,24 @@
 package br.edu.ifam.daic.filmes.recycler;
 
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import br.edu.ifam.daic.filmes.R;
+
 public class MovieViewHolder extends RecyclerView.ViewHolder {
 
 
+    private TextView textViewMovie;
+
     public MovieViewHolder(@NonNull View itemView) {
         super(itemView);
+        textViewMovie = itemView.findViewById(R.id.textViewMovie);
+    }
+
+    public TextView getTextViewMovie(){
+        return textViewMovie;
     }
 }
